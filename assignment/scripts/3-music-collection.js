0e6af21c3fa98bc4720914c4be250f360b2be7bb
Hannah Comparetto <hannahcomparetto@gmail.com>
2023-07-08 23:42:35 -0500

    add album function & 6 new albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,19 +3,21 @@ console.log('***** Music Collection *****')
 let collection = [];
 console.log('Items I have in my collection are:', collection);
 
-function addToCollection(album){
-album = {
-    title: 'Absolution',
-    artist: 'Muse',
-    yearPublished: 2003,
+function addToCollection(title, artist, yearPublished) {
+    let album = {
+        title,
+        artist,
+        yearPublished,
     }
-if (collection.length < 20){
+    if (collection.length < 20) {
         collection.push(album)
-     return album;}
-    else if(collection.length >= 20){
-        return false;
     }
+    return album;
 }
 
-
-console.log('albums in collection are:', collection)
+console.log('Muse album added:', addToCollection('Absolution', 'Muse', 2003))
+console.log('The Used album added:', addToCollection('In Love and Death', 'The Used', 2004))
+console.log('The Used album added:', addToCollection('Lies for the Liars', 'The Used', 2007))
+console.log('Muse album added:', addToCollection('The 2nd Law', 'Muse', 2012))
+console.log('Jon Bellion album added:', addToCollection('The Human Condition', 'Jon Bellion', 2016))
+console.log('Jon Bellion album added:', addToCollection('Glory Sound Prep', 'Jon Bellion', 2018))
