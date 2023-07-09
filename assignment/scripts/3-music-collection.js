ae1bc9b07be5a65e526fbf91791cd7fc14032ae5
Hannah Comparetto <hannahcomparetto@gmail.com>
2023-07-08 22:14:46 -0500

    troubleshooting addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,3 +2,20 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 console.log('Items I have in my collection are:', collection);
+
+function addToCollection(album){
+album = {
+    title: 'Absolution',
+    artist: 'Muse',
+    yearPublished: 2003,
+    }
+if (collection.length < 20){
+        collection.push(album)
+     return album;}
+    else if(collection.length >= 20){
+        return false;
+    }
+}
+
+
+console.log('albums in collection are:', collection)
